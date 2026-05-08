6df97574d6e610bc0e42ffbc5230ede52fe5f0e6
mathuranc1 <mathuran.chandramohan@dcmail.ca>
2026-05-07 22:46:18 -0400

    Changed url base name again

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/inft2200-capstone/',
+  base: '/inft2202-capstone/',
   plugins: [react()],
 })
